e0a7ad1d6f07241f7a5d16798dae24a90c31a5b0
linrongbin16 <linrongbin16@outlook.com>
2023-12-11 09:58:54 +0800

    feat(mvp): WIP

---
+++ test/gentags_spec.lua
@@ -0,0 +1,20 @@
+local cwd = vim.fn.getcwd()
+
+describe("gentags", function()
+  local assert_eq = assert.is_equal
+  local assert_true = assert.is_true
+  local assert_false = assert.is_false
+
+  before_each(function()
+    vim.api.nvim_command("cd " .. cwd)
+  end)
+
+  local github_actions = os.getenv("GITHUB_ACTIONS") == "true"
+
+  local gentags = require("gentags")
+  describe("[setup]", function()
+    it("test", function()
+      gentags.setup()
+    end)
+  end)
+end)
