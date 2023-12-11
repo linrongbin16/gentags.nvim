dad1398fd2516c041494d089aca935c332239020
linrongbin16 <linrongbin16@outlook.com>
2023-12-11 14:24:36 +0800

    chore

--- lua/gentags.lua
+++ lua/gentags.lua
@@ -2,7 +2,26 @@ local M = {}
 
 --- @alias gentags.Options table<any, any>
 --- @type gentags.Options
-local Defaults = {}
+local Defaults = {
+
+  -- enable debug mode
+  --
+  --- @type boolean
+  debug = false,
+
+  -- print logs to console (command line).
+  --
+  --- @type boolean
+  console_log = true,
+
+  -- write logs to file.
+  --
+  -- For Windows: `$env:USERPROFILE\AppData\Local\nvim-data\gentags.log`.
+  -- For *NIX: `~/.local/share/nvim/gentags.log`.
+  --
+  --- @type boolean
+  file_log = false,
+}
 
 --- @type gentags.Options
 local Configs = {}
