91c14def9f01600782f40258476d2db8a9e91447
linrongbin16 <linrongbin16@outlook.com>
2023-12-12 10:30:55 +0800

    chore

--- lua/gentags/commons/jsons.lua
+++ lua/gentags/commons/jsons.lua
@@ -1,14 +1,10 @@
 -- Json encode/decode
 
-local actboy168_json = require(
-  (vim.env._COMMONS_NVIM_MODULE_PREFIX or "") .. "commons.actboy168_json"
-)
-
 local M = {
   encode = (vim.fn.has("nvim-0.9") and vim.json ~= nil) and vim.json.encode
-    or actboy168_json.encode,
+    or require("commons.actboy168_json").encode,
   decode = (vim.fn.has("nvim-0.9") and vim.json ~= nil) and vim.json.decode
-    or actboy168_json.decode,
+    or require("commons.actboy168_json").decode,
 }
 
 return M
