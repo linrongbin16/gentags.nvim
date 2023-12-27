a9f5a4e261392b86e3e0874c840a522b684fd6f4
linrongbin16 <linrongbin16@outlook.com>
2023-12-27 18:50:54 +0800

    Update configs.lua

--- lua/gentags/configs.lua
+++ lua/gentags/configs.lua
@@ -8,14 +8,14 @@ local Defaults = {
 
   -- generate options for binary, e.g. ctags
   opts = {
-    c = { "-R", "--append=no" },
-    cpp = { "-R", "--append=no" },
-    lua = { "-R", "--append=no" },
-    markdown = { "-R", "--append=no" },
+    c = { "-R" },
+    cpp = { "-R" },
+    lua = { "-R" },
+    markdown = { "-R" },
   },
 
   -- generate options for binary, e.g. ctags
-  fallback_opts = { "-R", "--append=no" },
+  fallback_opts = { "-R" },
 
   -- excluded filetypes
   exclude_filetypes = {},
