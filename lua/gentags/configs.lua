e34da19435d535fae576937df86a89bc72b491fc
linrongbin16 <linrongbin16@outlook.com>
2024-03-06 16:36:55 +0800

    chore

--- lua/gentags/configs.lua
+++ lua/gentags/configs.lua
@@ -11,61 +11,61 @@ local Defaults = {
 
   -- ctags options
   ctags = {
-    "--tag-relative=never",
+    ["--tag-relative=never"] = true,
 
     -- Recommended Options:
-    --
+
     -- -- exclude logs
-    -- "--exclude=*.log",
+    -- ["--exclude=*.log"] = true,
     --
     -- -- exclude vcs
-    -- "--exclude=*.git",
-    -- "--exclude=*.svg",
-    -- "--exclude=*.hg",
+    -- ["--exclude=*.git"] = true,
+    -- ["--exclude=*.svg"] = true,
+    -- ["--exclude=*.hg"] = true,
     --
     -- -- exclude nodejs
-    -- "--exclude=node_modules",
+    -- ["--exclude=node_modules"] = true,
     --
     -- -- exclude tags/cscope
-    -- "--exclude=*tags*",
-    -- "--exclude=*cscope.*",
+    -- ["--exclude=*tags*"] = true,
+    -- ["--exclude=*cscope.*"] = true,
     --
     -- -- exclude python
-    -- "--exclude=*.pyc",
+    -- ["--exclude=*.pyc"] = true,
     --
     -- -- exclude jvm class
-    -- "--exclude=*.class",
+    -- ["--exclude=*.class"] = true,
     --
     -- -- exclude VS project generated
-    -- "--exclude=*.pdb",
-    -- "--exclude=*.sln",
-    -- "--exclude=*.csproj",
-    -- "--exclude=*.csproj.user",
+    -- ["--exclude=*.pdb"] = true,
+    -- ["--exclude=*.sln"] = true,
+    -- ["--exclude=*.csproj"] = true,
+    -- ["--exclude=*.csproj.user"] = true,
     --
     -- -- exclude blobs
-    -- "--exclude=*.exe",
-    -- "--exclude=*.dll",
-    -- "--exclude=*.mp3",
-    -- "--exclude=*.ogg",
-    -- "--exclude=*.flac",
-    -- "--exclude=*.swp",
-    -- "--exclude=*.swo",
-    -- "--exclude=*.bmp",
-    -- "--exclude=*.gif",
-    -- "--exclude=*.ico",
-    -- "--exclude=*.jpg",
-    -- "--exclude=*.png",
-    -- "--exclude=*.rar",
-    -- "--exclude=*.zip",
-    -- "--exclude=*.tar",
-    -- "--exclude=*.tar.gz",
-    -- "--exclude=*.tar.xz",
-    -- "--exclude=*.tar.bz2",
-    -- "--exclude=*.pdf",
-    -- "--exclude=*.doc",
-    -- "--exclude=*.docx",
-    -- "--exclude=*.ppt",
-    -- "--exclude=*.pptx",
+    -- ["--exclude=*.exe"] = true,
+    -- ["--exclude=*.dll"] = true,
+    -- ["--exclude=*.mp3"] = true,
+    -- ["--exclude=*.ogg"] = true,
+    -- ["--exclude=*.flac"] = true,
+    -- ["--exclude=*.swp"] = true,
+    -- ["--exclude=*.swo"] = true,
+    -- ["--exclude=*.bmp"] = true,
+    -- ["--exclude=*.gif"] = true,
+    -- ["--exclude=*.ico"] = true,
+    -- ["--exclude=*.jpg"] = true,
+    -- ["--exclude=*.png"] = true,
+    -- ["--exclude=*.rar"] = true,
+    -- ["--exclude=*.zip"] = true,
+    -- ["--exclude=*.tar"] = true,
+    -- ["--exclude=*.tar.gz"] = true,
+    -- ["--exclude=*.tar.xz"] = true,
+    -- ["--exclude=*.tar.bz2"] = true,
+    -- ["--exclude=*.pdf"] = true,
+    -- ["--exclude=*.doc"] = true,
+    -- ["--exclude=*.docx"] = true,
+    -- ["--exclude=*.ppt"] = true,
+    -- ["--exclude=*.pptx"] = true,
   },
 
   -- workspace detection
